6e5976f3670356687e1f1eea82024d44ff805155
Sumeet-Y1 <sumeety202@gmail.com>
2026-01-08 12:02:13 +0530

    Frontend added

---
+++ Stumange/src/main/java/SPringStudent/Stumange/En21Service.java
@@ -0,0 +1,39 @@
+package SPringStudent.Stumange;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class En21Service {
+
+    @Autowired
+    private En21Repository en21Repository;
+
+    public En21 saveStudent(En21 student) {
+        return en21Repository.save(student);
+    }
+
+    public List<En21> getAllStudents() {
+        return en21Repository.findAll();
+    }
+
+    public En21 getStudentById(Long id) {
+        return en21Repository.findById(id).orElse(null);
+    }
+
+    public En21 updateStudent(Long id, En21 updatedStudent) {
+        En21 student = en21Repository.findById(id).orElse(null);
+        if (student != null) {
+            student.setName(updatedStudent.getName());
+            student.setEmail(updatedStudent.getEmail());
+            student.setAge(updatedStudent.getAge());
+            return en21Repository.save(student);
+        }
+        return null;
+    }
+
+    public void deleteStudent(Long id) {
+        en21Repository.deleteById(id);
+    }
+}
